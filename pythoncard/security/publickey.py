0a3d086bfa594e8409c9c85df811e053b8ec0bc9
Benoît Allard <benoit.allard@gmx.de>
2011-01-18 19:52:05 +0100

    I guess I'm not tightly bound to pycrypto anymore: added an abstract layer

--- pythoncard/security/publickey.py
+++ pythoncard/security/publickey.py
@@ -1,20 +1,29 @@
 from python.lang import ArrayIndexOutOfBoundsException
+from pythoncard.framework import Util
 from pythoncard.security import CryptoException
 from pythoncard.security.key import Key, _longToArray, _arrayTolong
 
-from Crypto.PublicKey import RSA
-
 class PublicKey(Key):
     pass
+
+class RSAPublicKey(PublicKey):
+    def getExponent(self, buffer, offset):
+        raise NotImplementedError
+    
+    def	getModulus(self, buffer, offset):
+        raise NotImplementedError
+
+    def setExponent(self, buffer, offset, length):
+        raise NotImplementedError
+        
+    def setModulus(self, buffer, offset, length):
+        raise NotImplementedError
 
 def standardGetter(f):
     def getter(self, *args, **kwargs):
         if not self.isInitialized():
             raise CryptoException(CryptoException.UNINITIALIZED_KEY)
-        try:
-            return f(self, *args, **kwargs)
-        except IndexError:
-            raise ArrayIndexOutOfBoundsException()
+        return f(self, *args, **kwargs)
     return getter
 
 def standardSetter(f):
@@ -26,46 +35,43 @@ def standardSetter(f):
         f(self, *args, **kwargs)
         if (self.modulus is not None) and (self.exponent is not None):
             if self._theKey is None: #if not aready set, set it
-                self._theKey = RSA.construct([_arrayTolong(self.modulus),
-                                              _arrayTolong(self.exponent)])
+                self._construct();
             self.size = len(self.modulus)
             self._setInitialized()
     return setter
 
-class RSAPublicKey(PublicKey):
-
+class PyCryptoRSAPublicKey(RSAPublicKey):
+    from Crypto.PublicKey import RSA
     def __init__(self):
-        PublicKey.__init__(self)
+        RSAPublicKey.__init__(self)
         self.exponent = None
         self.modulus = None
         self._theKey = None
 
     @standardGetter
     def getExponent(self, buffer, offset):
-        for i in range(len(self.exponent)):
-            buffer[offset+i] = self.exponent[i]
+        Util.arrayCopy(self.exponent, 0, buffer, offset, len(self.exponent))
         return len(self.exponent)
     
     @standardGetter
     def getModulus(self, buffer, offset):
-        for i in range(len(self.modulus)):
-            buffer[offset+i] = self.modulus[i]
+        Util.arrayCopy(self.modulus, 0, buffer, offset, len(self.modulus))
         return len(self.modulus)
 
     @standardSetter
     def setExponent(self, buffer, offset, length):
-        self.exponent = []
-        for b in buffer[offset:offset+length]:
-            self.exponent.append(b)
+        self.exponent = buffer[offset:offset+length]
 
     @standardSetter
     def setModulus(self, buffer, offset, length):
-        self.modulus = []
-        for b in buffer[offset:offset+length]:
-            self.modulus.append(b)
+        self.modulus = buffer[offset:offset+length]
 
     @standardSetter
     def setTheKey(self, theKey):
         self._theKey = theKey
         self.exponent = _longToArray(theKey.e)
         self.modulus = _longToArray(theKey.n)
+
+    def _construct(self):
+        self._theKey = RSA.construct([_arrayTolong(self.modulus),
+                                      _arrayTolong(self.exponent)])
