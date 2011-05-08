6e188f32a5e64f5dc92e65ec1caab80542d5c730
Benoît Allard <benoit.allard@gmx.de>
2011-05-09 00:28:27 +0200

    Update Random tests

--- test/testRandom.py
+++ test/testRandom.py
@@ -1,24 +1,24 @@
 import unittest
 
-from pythoncard.security import Random, CryptoException
+from pythoncard.security import RandomData, CryptoException
 
 class testRandom(unittest.TestCase):
 
     def testgetInstance(self):
         try:
-            rdm = Random.getInstance(Random.ALG_SECURE_RANDOM)
+            rdm = RandomData.getInstance(RandomData.ALG_SECURE_RANDOM)
         except CryptoException:
             self.fail()
             
         try:
-            rdm = Random.getInstance(Random.ALG_PSEUDO_RANDOM)
+            rdm = RandomData.getInstance(RandomData.ALG_PSEUDO_RANDOM)
             self.fail()
         except CryptoException, ce:
             if ce.getReason() != CryptoException.NO_SUCH_ALGORITHM:
                 self.fail()
 
     def testGeneratedData(self):
-        rdm = Random.getInstance(Random.ALG_SECURE_RANDOM)
+        rdm = RandomData.getInstance(RandomData.ALG_SECURE_RANDOM)
         buf = [0 for i in range(50)]
         rdm.generateData(buf, 0, 25)
         self.assertEquals([0 for i in range(25)], buf[25:])
