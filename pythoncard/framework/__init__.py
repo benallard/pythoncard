18a90c3c72a25fe5f02539cfc6e65f18ad8bc732
Benoit Allard <benoit@aeteurope.nl>
2011-07-26 10:33:09 +0200

    Correct Exceptions inheritance tree.

--- pythoncard/framework/__init__.py
+++ pythoncard/framework/__init__.py
@@ -11,13 +11,15 @@ from pythoncard.framework import Applet
 
 """
 
+from python.lang import RuntimeException
+
 class CardException(Exception):
     pass
 
-class UserException(Exception):
+class UserException(CardException):
     pass
 
-class CardRuntimeException(Exception):
+class CardRuntimeException(RuntimeException):
     def __init__(self, reason):
         self._reason = reason
 
