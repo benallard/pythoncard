29fe43292538279a6bc3fc6241eb3bb482de9dae
Benoit Allard <benoit@aeteurope.nl>
2011-09-19 22:57:30 +0200

    Add a test for the keyBuilder

---
+++ test/testKeyBuilder.py
@@ -0,0 +1,9 @@
+import unittest
+
+from pythoncard.security import KeyBuilder
+
+class testKeyBuilder(unittest.TestCase):
+
+    def testLength(self):
+        key = KeyBuilder.buildKey(KeyBuilder.TYPE_RSA_PUBLIC, KeyBuilder.LENGTH_RSA_768, False)
+        self.assertEquals(768, key.getSize())
