5e20c1769f0772f08b832b4c11bee6abd90bc540
Benoît Allard <benoit.allard@gmx.de>
2011-09-22 21:18:57 +0200

    Improve int and TLV support

--- pythoncardx/framework/util/intx.py
+++ pythoncardx/framework/util/intx.py
@@ -13,18 +13,20 @@ def signed(value, depth):
 def s4(i):
     return signed(i, 4)
 
+def s1(b):
+    return signed(b, 1)
+
 def sanatize(array):
     for i in range(len(array)):
-        array[i] = signed(array[i], 1)
+        array[i] = s1(array[i])
 
 class JCint(object):
 
     @staticmethod
-    
     def getInt(bArray, bOff):
-        i =  bArray[bOff] << 24 + \
-            bArray[bOff + 1] << 16 + \
-            bArray[bOff + 2] << 8 + \
+        i =  (bArray[bOff] << 24) + \
+            (bArray[bOff + 1] << 16) + \
+            (bArray[bOff + 2] << 8) + \
             bArray[bOff + 3]
         return s4(i)
     
@@ -32,11 +34,11 @@ class JCint(object):
     def makeInt(*args):
         if len(args) == 4:
             (b1, b2, b3, b4) = args
-            i = b1 << 24 + b2 << 16 + b3 << 8 + b4
+            i = (b1 << 24) + (b2 << 16) + (b3 << 8) + b4
             return s4(i)
         else:
             (s1, s2) = args
-            i = s1 << 16 + s2
+            i = (s1 << 16) + s2
             return s4(i)
 
     @staticmethod
@@ -48,8 +50,8 @@ class JCint(object):
     def setInt(bArray, bOff, iValue1, iValue2):
         # ints are transmited as two params ...
         iVal = JCint.makeInt(iValue1, iValue2)
-        bArray[bOff] = (iVal >> 24) & 0xff
-        bArray[bOff + 1] = iVal >> 16 & 0xff
-        bArray[bOff + 2] = iVal >> 8 & 0xff
-        bArray[bOff + 3] = iVal & 0xff
+        bArray[bOff] = s1((iVal >> 24) & 0xff)
+        bArray[bOff + 1] = s1((iVal >> 16) & 0xff)
+        bArray[bOff + 2] = s1((iVal >> 8) & 0xff)
+        bArray[bOff + 3] = s1(iVal & 0xff)
         return bOff + 4
