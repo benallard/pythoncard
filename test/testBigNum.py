55ecb56aea053e43f47126d9357ff81feb704fed
Benoit Allard <benoit@aeteurope.nl>
2011-09-27 20:22:01 +0200

    Add tests, correct issues

---
+++ test/testBigNum.py
@@ -0,0 +1,29 @@
+import unittest
+
+from pythoncardx.framework.math import BigNumber
+
+class BigNumTest(unittest.TestCase):
+
+    def testInitBCD(self):
+        bn = BigNumber(8)
+        bn.init([-128, 0x00], 0, 2, BigNumber.FORMAT_BCD)
+        self.assertEquals(8000, bn._value)
+
+    def testInitHEX(self):
+        bn = BigNumber(8)
+        bn.init([0x10, 0x00], 0, 2, BigNumber.FORMAT_HEX)
+        self.assertEquals(4096, bn._value)
+
+    def testtoBytesBCD(self):
+        bn = BigNumber(8)
+        bn.init([0x12, -128], 0, 2, BigNumber.FORMAT_BCD)
+        array = [0 for i in range(3)]
+        bn.toBytes(array, 0, 2, BigNumber.FORMAT_BCD)
+        self.assertEquals([0x12, -128], array[0:2])
+
+    def testtoBytesHEX(self):
+        bn = BigNumber(8)
+        bn.init([-127, 0x34], 0, 2, BigNumber.FORMAT_HEX)
+        array = [0 for i in range(3)]
+        bn.toBytes(array, 0, 2, BigNumber.FORMAT_HEX)
+        self.assertEquals([-127, 0x34], array[0:2])
--- test/testTLVTLV.py
+++ test/testTLVTLV.py
@@ -1,5 +1,7 @@
 import unittest
 
+from test.tlvtag import Tag, TLV
+
 from pythoncardx.framework.tlv import BERTag, PrimitiveBERTag, ConstructedBERTag, \
                                       BERTLV, PrimitiveBERTLV, ConstructedBERTLV
 
@@ -8,13 +10,124 @@ class BERTLVTest(unittest.TestCase):
     def testConstrInit(self):
         tag = ConstructedBERTag()
         tag.init(3, 1)
-        self.assertEquals(3, tag._tagClass)
-        self.assertEquals(1, tag._PC)
-        self.assertEquals(1, tag._tagNumber)
         array = [0 for i in range(10)]
         tag.toBytes(array, 0)
         tlv = BERTLV.getInstance(array, 0, 2)
         self.assertEquals(tag, tlv.getTag())
+
+class BERTLVfindTest(unittest.TestCase):
+
+    def testfindOk(self):
+        tlv = BERTLV.getInstance(
+            TLV(Tag(constr=True, number=8),
+                 TLV(Tag(number=7), [0,6,7,56]) +
+                 TLV(Tag(cls=2, number=5), [89,4,3,6,1,6,])
+                 ),
+            0)
+
+        tlv2 = tlv.find(BERTag.getInstance(Tag(number=7), 0))
+        self.assertEquals(BERTag.getInstance(Tag(number=7), 0), tlv2.getTag())
+        array = [0 for i in range (10)]
+        size = tlv2.getValue(array, 0)
+        self.assertEquals(4, size)
+        self.assertEquals([0, 6, 7, 56], array[0:size])
+
+        tlv2 = tlv.find(BERTag.getInstance(Tag(cls=2, number=5), 0))
+        self.assertEquals(BERTag.getInstance(Tag(cls=2, number=5), 0), tlv2.getTag())
+        size = tlv2.getValue(array, 0)
+        self.assertEquals(6, size)
+        self.assertEquals([89, 4, 3, 6, 1, 6], array[0:size])
+
+    def testfindNOK(self):
+        tlv = BERTLV.getInstance(
+            TLV(Tag(constr=True, number=8),
+                 TLV(Tag(number=7), [0,6,7,56]) +
+                 TLV(Tag(cls=2, number=5), [89,4,3,6,1,6,])
+                 ),
+            0)
+
+        self.assertEquals(None, tlv.find(BERTag.getInstance(Tag(number=3), 0)))
+
+    def testfindFirst(self):
+        tlv = BERTLV.getInstance(
+            TLV(Tag(constr=True, number=8),
+                 TLV(Tag(number=7), [0,6,7,56]) +
+                 TLV(Tag(cls=2, number=5), [89,4,3,6,1,6,])
+                 ),
+            0)
+
+        tlv2 = tlv.find(None)
+        self.assertEquals(BERTag.getInstance(Tag(number=7), 0), tlv2.getTag())
+        array = [0 for i in range (10)]
+        size = tlv2.getValue(array, 0)
+        self.assertEquals(4, size)
+        self.assertEquals([0, 6, 7, 56], array[0:size])
+
+    def testfindFirstNOK(self):
+        tlv = BERTLV.getInstance(
+            TLV(Tag(constr=True, number=17)),
+            0)
+
+        self.assertEquals(None, tlv.find(None))
+
+class BERTLVfindNextTest(unittest.TestCase):
+
+    def testfindNextOK(self):
+        tag1 = Tag(number=1)
+        tag2 = Tag(cls=2, number=2)
+        tlv = BERTLV.getInstance(
+            TLV(Tag(constr=True),
+                 TLV(tag1, [0,6,7,56]) + # tlv2
+                 TLV(tag2, [89,4,3,6,1,6,]) +
+                 TLV(tag2, [0,6,7,56]) +
+                 TLV(tag1, [89,4,3,3,1,6,7,8]) + # tlv3
+                 TLV(tag2, [0,6,7,0, 0, 0, 0]) +
+                 TLV(tag1, [0,6,7,56]) + # tlv2 (bis)
+                 TLV(tag1, [89,4]) # tlv4
+                 ),
+            0)
+
+        tlv2 = tlv.find(BERTag.getInstance(tag1, 0))
+        tlv3 = tlv.findNext(
+            BERTag.getInstance(tag1, 0),
+            tlv2,
+            1)
+
+        self.assertEquals(BERTag.getInstance(tag1, 0), tlv3.getTag())
+        array = [0 for i in range (10)]
+        size = tlv3.getValue(array, 0)
+        self.assertEquals(8, size)
+        self.assertEquals([89,4,3,3,1,6,7,8], array[0:size])
+
+        tlv4 = tlv.findNext(
+            BERTag.getInstance(tag1, 0),
+            tlv2,
+            2)
+        self.assertEquals(BERTag.getInstance(tag1, 0), tlv4.getTag())
+        size = tlv4.getValue(array, 0)
+        self.assertEquals(2, size)
+        self.assertEquals([89,4], array[0:size])
+
+    def testfindNextNOK(self):
+        tag1 = Tag(number=1)
+        tag2 = Tag(cls=2, number=2)
+        tlv = BERTLV.getInstance(
+            TLV(Tag(constr=True),
+                 TLV(tag1, [0,6,7,56]) + # tlv2
+                 TLV(tag2, [89,4,3,6,1,6,]) +
+                 TLV(tag2, [0,6,7,56]) +
+                 TLV(tag1, [89,4,3,3,1,6,7,8]) +
+                 TLV(tag2, [0,6,7,0, 0, 0, 0]) +
+                 TLV(tag1, [0,6,7,56]) + # tlv2 (bis)
+                 TLV(tag1, [89,4])
+                 ),
+            0)
+
+        tlv2 = tlv.find(BERTag.getInstance(tag1, 0))
+        self.assertEquals(None, tlv.findNext(
+            BERTag.getInstance(Tag(number=5), 0),
+            tlv2,
+            1))
 
 class StaticTest(unittest.TestCase):
 
